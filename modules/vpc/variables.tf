cb5e8d4afdc3a76f8ebf9b2e3a15e02e50f5b3b9
ahmedamr91 <62996024+ahmedamr91@users.noreply.github.com>
2026-07-23 11:04:23 +0300

    feat(vpc): create reusable VPC module

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,29 @@
+variable "project_name" {
+  description = "Project name"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR Block"
+  type        = string
+}
+
+variable "public_subnets" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "Availability Zones"
+  type        = list(string)
+}
