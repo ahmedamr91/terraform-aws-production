e1acd860dba587ef2353f96cc848bc2404af5f71
ahmedamr91 <62996024+ahmedamr91@users.noreply.github.com>
2026-07-23 10:56:37 +0300

    chore: initialize terraform project structure

--- variables.tf
+++ variables.tf
@@ -1,7 +1,20 @@
 variable "aws_region" {
+  type        = string
+  description = "AWS Region"
+
   default = "us-east-1"
 }
 
 variable "project_name" {
-  default = "production-app"
+  type        = string
+  description = "Project Name"
+
+  default = "terraform-aws-production"
+}
+
+variable "environment" {
+  type        = string
+  description = "Deployment Environment"
+
+  default = "dev"
 }
